ba78502be086c944ab329dca4a9b8599c1397acb
youngpark671 <youngpark_671@163.com>
2023-06-10 23:09:22 +0800

    更新：水面和浪花动态贴图

--- app/src/main/cpp/maps/active_tile_bmp.h
+++ app/src/main/cpp/maps/active_tile_bmp.h
@@ -5,4 +5,10 @@
 #ifndef METALMAX_ACTIVE_TILE_BMP_H
 #define METALMAX_ACTIVE_TILE_BMP_H
 
+#include "../global.h"
+
+extern byte wave[3][16 * 4];
+
+extern byte water[3][16 * 16];
+
 #endif //METALMAX_ACTIVE_TILE_BMP_H
