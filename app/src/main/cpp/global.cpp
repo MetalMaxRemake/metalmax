79138cb69722d7484ae8d278d8e9944141e1adec
park671 <youngpark_671@163.com>
2024-09-20 17:31:54 +0800

    feature: add time print on log

--- app/src/main/cpp/global.cpp
+++ app/src/main/cpp/global.cpp
@@ -6,9 +6,32 @@
 #include <unistd.h>
 #include "global.h"
 
+char *get_current_time_str() {
+    static char buffer[30];
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    struct tm *local = localtime(&tv.tv_sec);
+
+    snprintf(buffer, sizeof(buffer), "%04d-%02d-%02d %02d:%02d:%02d.%03d",
+             local->tm_year + 1900,
+             local->tm_mon + 1,
+             local->tm_mday,
+             local->tm_hour,
+             local->tm_min,
+             local->tm_sec,
+             tv.tv_usec / 1000);
+
+    return buffer;
+}
+
+
 void logd(char *tag, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
+    if (false) {
+        get_current_time_str();
+    }
     __android_log_print(ANDROID_LOG_DEBUG, tag, fmt, args);
     va_end(args);
 }
@@ -16,6 +39,9 @@ void logd(char *tag, const char *fmt, ...) {
 void loge(char *tag, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
+    if (false) {
+        get_current_time_str();
+    }
     __android_log_print(ANDROID_LOG_ERROR, tag, fmt, args);
     va_end(args);
 }
